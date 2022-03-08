e4b8649c308d8be9e26ef785970615444e5d1a7a
Adam Congdon <cac8907@gmail.com>
2022-03-08 11:58:04 -0500

    updated output dirs

--- HC/HC_Reporting/CVariables.cs
+++ HC/HC_Reporting/CVariables.cs
@@ -11,8 +11,8 @@ namespace VeeamHealthCheck
     class CVariables
     {
         public readonly string OutDir = @"C:\temp\vHC";
-        public static string safeDir = @"C:\temp\vHC\Scrubbed";
-        public static string unsafeDir = @"C:\temp\vHC\Contains_Sensitive_Data";
+        public static string safeDir = @"C:\temp\vHC\Anonymous";
+        public static string unsafeDir = @"C:\temp\vHC\Original";
         public static string desiredDir { get; set; }
         public  string unSafeDir2()
         {
