3f9f7a674bb8cbcb959c0a8be7a2de6d20570a08
Adam Congdon <adam.congdon@veeam.com>
2024-03-15 11:28:23 -0400

    added ignored file + added logic for VBR-specific function

---
+++ vHC/HC_Reporting/Functions/Reporting/Html/VBR/VbrTables/BackupServer/CConfigBackupTable.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) 2021, Adam Congdon <adam.congdon2@gmail.com>
+// MIT License
+using System;
+using VeeamHealthCheck.Functions.Analysis.DataModels;
+using VeeamHealthCheck.Functions.Collection.Security;
+using VeeamHealthCheck.Functions.Reporting.Html;
+using VeeamHealthCheck.Functions.Reporting.Html.Shared;
+
+namespace VeeamHealthCheck.Reporting.Html.VBR
+{
+    internal class CConfigBackupTable
+    {
+        CHtmlFormatting _form = new();
+        public CConfigBackupTable()
+        {
+            ParseBackupServer();
+        }
+        public Tuple<string, string> ConfigBackupEnabled()
+        {
+            string header = _form.TableHeader("Config Backup Enabled", "");
+            string data = _form.TableData(CSecurityGlobalValues.ConfigBackupEnabled.ToString(), "");
+
+            return Tuple.Create(header, data);
+        }
+        public Tuple<string, string> ConfigBackupSuccessful()
+        {
+            string header = _form.TableHeader("Config Backup Last Run Successful", "");
+            string data = _form.TableData(CSecurityGlobalValues.ConfigBackupSuccess.ToString(), "");
+
+            return Tuple.Create(header, data);
+        }
+        public Tuple<string, string> ConfigBackupEncrypted()
+        {
+            string header = _form.TableHeader("Config Backup Encrypted", "");
+            string data = _form.TableData(CSecurityGlobalValues.ConfigBackupEncrypted.ToString(), "");
+
+            return Tuple.Create(header, data);
+        }
+        private void ParseBackupServer()
+        {
+            CDataFormer df = new();
+            Functions.Analysis.DataModels.BackupServer b = df.BackupServerInfoToXml(false);
+            CSecurityGlobalValues.ConfigBackupEnabled = b.ConfigBackupEnabled;
+            CSecurityGlobalValues.ConfigBackupEncrypted = b.ConfigBackupEncryption;
+            CSecurityGlobalValues.ConfigBackupSuccess = b.ConfigBackupLastResult;
+
+        }
+    }
+}
