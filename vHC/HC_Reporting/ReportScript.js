3250aeb82fbe73f7ee6e7e00a751c53b218336ba
Adam Congdon <adam.congdon@veeam.com>
2024-08-26 12:27:01 -0400

    updated JS to properly sort number as numbers

--- vHC/HC_Reporting/ReportScript.js
+++ vHC/HC_Reporting/ReportScript.js
@@ -56,28 +56,37 @@ function test() {
 
 }
 function sortTableByColumn(table, column, asc = true) {
-	const dirModifier = asc ? 1 : -1;
-	const tBody = table.tBodies[0];
-	const rows = Array.from(tBody.querySelectorAll("tr"));
-	const sortedRows = rows.sort((a, b) => {
-		const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-		const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
+    const sortedRows = rows.sort((a, b) => {
+        const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+        const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
 
-		return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
-	});
+        const aStartsWithNumber = /^\d/.test(aColText);
+        const bStartsWithNumber = /^\d/.test(bColText);
 
-	// Remove all existing TRs from the table
-	while (tBody.firstChild) {
-		tBody.removeChild(tBody.firstChild);
-	}
+        if (aStartsWithNumber && bStartsWithNumber) {
+            const aColNumber = parseFloat(aColText);
+            const bColNumber = parseFloat(bColText);
+            return (aColNumber - bColNumber) * dirModifier;
+        } else {
+            return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+        }
+    });
 
-	// Re-add the newly sorted rows
-	tBody.append(...sortedRows);
+    // Remove all existing TRs from the table
+    while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+    }
 
-	// Remember how the column is currently sorted
-	table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
-	table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
-	table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
+    // Re-add the newly sorted rows
+    tBody.append(...sortedRows);
+
+    // Remember how the column is currently sorted
+    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
 }
 
 document.querySelectorAll(".table-sortable th").forEach(headerCell => {
