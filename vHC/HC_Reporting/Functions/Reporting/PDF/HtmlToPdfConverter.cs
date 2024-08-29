6d42c3415e824c477cf90c69eb1dd2dfbc54f653
Blake Sheffield <blakeshef1@gmail.com>
2024-08-29 17:52:52 -0400

    used class converter vs method converter in HtmlToPdfConverter, Added Namespace to match rest of the program, Corrected usage to include the namespace

--- vHC/HC_Reporting/Functions/Reporting/PDF/HtmlToPdfConverter.cs
+++ vHC/HC_Reporting/Functions/Reporting/PDF/HtmlToPdfConverter.cs
@@ -6,45 +6,48 @@ using System.Drawing.Printing;
 using System.IO;
 using System.Windows.Controls;
 
-public class HtmlToPdfConverter
-{
-    private IConverter _converter;
-
-    public HtmlToPdfConverter()
+namespace VeeamHealthCheck.Functions.Reporting.Pdf
+{ 
+    public class HtmlToPdfConverter
     {
-        _converter = new SynchronizedConverter(new PdfTools());
-    }
+        private IConverter _converter;
 
-    [STAThread]
-    public void ConvertHtmlToPdf(string htmlContent, string outputPath)
-    {
-        var converter = new SynchronizedConverter(new PdfTools());
-        var html = htmlContent; //"<h1>Hello, World!</h1>"; // replace with your HTML string
-        var doc = new HtmlToPdfDocument()
+        public HtmlToPdfConverter()
         {
-            GlobalSettings = {
-            ColorMode = DinkToPdf.ColorMode.Color,
-            Orientation = DinkToPdf.Orientation.Landscape,
-            PaperSize = DinkToPdf.PaperKind.A3,
-            Margins = new MarginSettings { Top = 10 },
-    },
-            Objects = {
-        new ObjectSettings()
+            _converter = new SynchronizedConverter(new PdfTools());
+        }
+
+        [STAThread]
+        public void ConvertHtmlToPdf(string htmlContent, string outputPath)
         {
-            HtmlContent = html,
-            WebSettings = { DefaultEncoding = "utf-8" },
+            var html = htmlContent; //"<h1>Hello, World!</h1>"; // replace with your HTML string
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    ColorMode = DinkToPdf.ColorMode.Color,
+                    Orientation = DinkToPdf.Orientation.Landscape,
+                    PaperSize = DinkToPdf.PaperKind.A3,
+                    Margins = new MarginSettings { Top = 10 },
+                },
+                Objects = {
+                    new ObjectSettings()
+                    {
+                        HtmlContent = html,
+                        WebSettings = { DefaultEncoding = "utf-8" },
+                    }
+                }
+            };
+
+            byte[] pdf = _converter.Convert(doc);
+            File.WriteAllBytes(outputPath, pdf);
+
+        }
+        // dispose method
+        public void Dispose()
+        {
+            _converter = null;
         }
     }
-        };
-
-        byte[] pdf = converter.Convert(doc);
-        File.WriteAllBytes(outputPath, pdf);
-        
-    }
-    // dispose method
-    public void Dispose()
-    {
-        _converter = null;
-    }
 }
+
 
