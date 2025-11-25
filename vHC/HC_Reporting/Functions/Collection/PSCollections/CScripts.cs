0ef19eea9573b234141782af2fa0ae802d11d894
Adam Congdon <adam.congdon@veeam.com>
2025-11-25 12:24:59 -0500

    Update project files and tasks for consistency and version increment to 3.0.0.119

--- vHC/HC_Reporting/Functions/Collection/PSCollections/CScripts.cs
+++ vHC/HC_Reporting/Functions/Collection/PSCollections/CScripts.cs
@@ -1,53 +1,60 @@
-﻿using System;
+﻿// <copyright file="CScripts.cs" company="AdamCongdon">
+// Copyright (c) AdamCongdon. All rights reserved.
+// </copyright>
+
+using System;
 using System.Management.Automation;
 
 namespace VeeamHealthCheck.Functions.Collection.PSCollections
 {
     internal class CScripts
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CScripts"/> class.
+        /// </summary>
         public CScripts()
         {
-            //RunPsScript(GetJobInfo());
-            //RunPsScript("Get-VBRJob");
-            //RunPsScript(File.ReadAllText(Environment.CurrentDirectory + @"\Tools\Scripts\Get-VBRConfig.ps1"));
-        }
-        //script to get vbr-job info
-        public string GetJobInfo()
-        {
-            string script = @"
-            Connect-VBRServer -Server localhost
-            $jobs = Get-VBRJob
-            $piJob = Get-VBRPluginJob
-            $jobInfo = @()
-            foreach ($job in $jobs)
-            {
-                $jobInfo += $job
-            }
-
-            $jobInfo
-            ";
-            return script;
+            // RunPsScript(GetJobInfo());
+            // RunPsScript("Get-VBRJob");
+            // RunPsScript(File.ReadAllText(Environment.CurrentDirectory + @"\Tools\Scripts\Get-VBRConfig.ps1"));
         }
 
-        //script to get job information from Veeam PowerShell
-        //write a powershell script to save job information in a custom object containing members: jobName, jobType, sourceSizeUsedGB, sourceSizeProvisionedGb, Repository, daysOrPoints, retentionValue, GfsEnabled, GfsWeeklyCount, GfsMonthlyCount, GfsYearlyCount, SyntheticEnabled, ActiveFullEnabled, SyntheticDays, ActiveFullDays, compressionLevel, BlockSize, Encrypted, IndexingEnabled, schedule
+        // script to get vbr-job info
+        // public string GetJobInfo()
+        // {
+        //     string script = @"
+        //     Connect-VBRServer -Server localhost
+        //     $jobs = Get-VBRJob
+        //     $piJob = Get-VBRPluginJob
+        //     $jobInfo = @()
+        //     foreach ($job in $jobs)
+        //     {
+        //         $jobInfo += $job
+        //     }
 
-        //execute powershell script returned from GetJobInfo() method
+        //     $jobInfo
+        //     ";
+        //     return script;
+        // }
 
-        private void RunPsScript(string script)
-        {
-            using (PowerShell ps = PowerShell.Create())
-            {
-                ps.AddScript(script);
-                ps.AddParameter("VBRServer", "localhost");//.AddParameter("localhost");
-                ps.AddParameter("VBRVersion", "12");//.AddParameter("12");
-                var results = ps.Invoke();
-                foreach (var result in results)
-                {
-                    //do something with the results
-                    Console.WriteLine(result);
-                }
-            }
-        }
+        // script to get job information from Veeam PowerShell
+        // write a powershell script to save job information in a custom object containing members: jobName, jobType, sourceSizeUsedGB, sourceSizeProvisionedGb, Repository, daysOrPoints, retentionValue, GfsEnabled, GfsWeeklyCount, GfsMonthlyCount, GfsYearlyCount, SyntheticEnabled, ActiveFullEnabled, SyntheticDays, ActiveFullDays, compressionLevel, BlockSize, Encrypted, IndexingEnabled, schedule
+
+        // execute powershell script returned from GetJobInfo() method
+        // private void RunPsScript(string script)
+        // {
+        //     using (PowerShell ps = PowerShell.Create())
+        //     {
+        //         ps.AddScript(script);
+        //         ps.AddParameter("VBRServer", "localhost");// .AddParameter("localhost");
+        //         ps.AddParameter("VBRVersion", "12");// .AddParameter("12");
+        //         var results = ps.Invoke();
+        //         foreach (var result in results)
+        //         {
+        //             // do something with the results
+        //             Console.WriteLine(result);
+        //         }
+        //     }
+        // }
     }
 }
